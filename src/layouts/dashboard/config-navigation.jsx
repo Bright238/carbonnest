14c78b14bbacbef3ee381a0b7a9138e9b31a643f
LisaChileshe <lisamari042@gmail.com>
2024-05-09 12:25:28 +0200

    added paths for vca, hts and mother-index

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -13,30 +13,26 @@ const navConfig = [
     icon: icon('ic_analytics'),
   },
   {
-    title: 'user',
+    title: 'Household Register',
     path: '/user',
     icon: icon('ic_user'),
   },
   {
-    title: 'product',
-    path: '/products',
-    icon: icon('ic_cart'),
+    title: 'VCA Register',
+    path: '/vca',
+    icon: icon('ic_user'),
   },
   {
-    title: 'blog',
-    path: '/blog',
-    icon: icon('ic_blog'),
+    title: 'Mother Index Register',
+    path: '/mother-index',
+    icon: icon('ic_user'),
   },
   {
-    title: 'login',
-    path: '/login',
-    icon: icon('ic_lock'),
+    title: 'HTS',
+    path: '/hts',
+    icon: icon('ic_user'),
   },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: icon('ic_disabled'),
-  },
+ 
 ];
 
 export default navConfig;
