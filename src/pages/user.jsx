4e5939b9225c721d83a9636eca87add2b321d6e4
LisaChileshe <lisamari042@gmail.com>
2024-05-09 12:38:02 +0200

    changed heading to match current page

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -8,7 +8,7 @@ export default function UserPage() {
   return (
     <>
       <Helmet>
-        <title> User | Minimal UI </title>
+        <title> Household | Ecap+ PMP </title>
       </Helmet>
 
       <UserView />
